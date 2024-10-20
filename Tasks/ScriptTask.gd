dc2ef1f969ff85be50e9e03c8b7f6096f996d0c6
kobewi <kobewi4e@gmail.com>
2024-10-20 23:17:50 +0200

    Add script validation for ScriptTask

--- Tasks/ScriptTask.gd
+++ Tasks/ScriptTask.gd
@@ -14,7 +14,22 @@ func _get_arguments() -> PackedStringArray:
 		ret.append(Data.get_res_path())
 	
 	ret.append("--script")
-	ret.append("res://Scripts/Tools/%s" % script_name)
+	ret.append(get_script_path())
 	
 	ret.append("--")
 	return ret
+
+func _prevalidate() -> bool:
+	if script_name.is_empty():
+		error_message = "ScriptTask's script name is empty. Assign it in the scene."
+		return false
+	
+	var script_path := get_script_path()
+	if not ResourceLoader.exists(script_path):
+		error_message = "The provided ScriptTask's script does not exist at expected path: \"%s\"." % script_path
+		return false
+	
+	return true
+
+func get_script_path() -> String:
+	return "res://Scripts/Tools/%s" % script_name
