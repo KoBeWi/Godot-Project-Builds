f8df046416ee1b2fbd763003601c688f218d2fc0
svetogam <sviatoslavsokolov@proton.me>
2024-11-04 12:20:33 -0800

    Add clear directory files test

--- Tests/TestExecution.gd
+++ Tests/TestExecution.gd
@@ -6,6 +6,7 @@ const PROJECTS := {
 const ROUTINES := {
 	# TestProject1
 	"copy_files": 0,
+	"clear_directory_files": 1,
 }
 const Scene := preload("res://Scenes/Execution.tscn")
 var scene: Node
@@ -43,3 +44,28 @@ func test_copy_files():
 	# Cleanup
 	DirAccess.remove_absolute(PROJECTS[1] + "file1-copy.txt")
 	DirAccess.remove_absolute(PROJECTS[1] + "EmptyDir/file1-copy.txt")
+
+func test_clear_directory_files():
+	# Check assumptions
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "MessyDir/a.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "MessyDir/b.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "MessyDir/Dir/c.txt"))
+	
+	# Setup
+	DirAccess.copy_absolute(PROJECTS[1] + "MessyDir/a.txt", PROJECTS[1] + "EmptyDir/a.txt")
+	DirAccess.copy_absolute(PROJECTS[1] + "MessyDir/b.txt", PROJECTS[1] + "EmptyDir/b.txt")
+	Data.load_project(PROJECTS[1])
+	Data.current_routine = Data.routines[ROUTINES.clear_directory_files]
+	
+	# Execute
+	add_child_autofree(scene)
+	await wait_seconds(1.5)
+	
+	# Check results
+	assert_false(FileAccess.file_exists(PROJECTS[1] + "MessyDir/a.txt"))
+	assert_false(FileAccess.file_exists(PROJECTS[1] + "MessyDir/b.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "MessyDir/Dir/c.txt"))
+
+	# Cleanup
+	DirAccess.rename_absolute(PROJECTS[1] + "EmptyDir/a.txt", PROJECTS[1] + "MessyDir/a.txt")
+	DirAccess.rename_absolute(PROJECTS[1] + "EmptyDir/b.txt", PROJECTS[1] + "MessyDir/b.txt")
