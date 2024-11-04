6fb4172f71605c75e4bde20bea84a3186d45c8c1
svetogam <sviatoslavsokolov@proton.me>
2024-11-04 11:45:40 -0800

    Refactor test for easier extensibility

--- Tests/TestExecution.gd
+++ Tests/TestExecution.gd
@@ -1,5 +1,12 @@
 extends GutTest
 
+const PROJECTS := {
+	1: "res://Tests/Projects/TestProject1/",
+}
+const ROUTINES := {
+	# TestProject1
+	"copy_files": 0,
+}
 const Scene := preload("res://Scenes/Execution.tscn")
 var scene: Node
 var original_exec_delay
@@ -15,26 +22,24 @@ func after_all():
 	Data.global_config["execution_delay"] = original_exec_delay
 
 func test_copy_files():
-	const project_path = "res://Tests/Projects/TestProject1/"
-	
 	# Check assumptions
-	assert_true(FileAccess.file_exists(project_path + "file1.txt"))
-	assert_false(FileAccess.file_exists(project_path + "file1-copy.txt"))
-	assert_false(FileAccess.file_exists(project_path + "EmptyDir/file1-copy.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "file1.txt"))
+	assert_false(FileAccess.file_exists(PROJECTS[1] + "file1-copy.txt"))
+	assert_false(FileAccess.file_exists(PROJECTS[1] + "EmptyDir/file1-copy.txt"))
 	
 	# Setup
-	Data.load_project(project_path)
-	Data.current_routine = Data.routines[0]
+	Data.load_project(PROJECTS[1])
+	Data.current_routine = Data.routines[ROUTINES.copy_files]
 	
 	# Execute
 	add_child_autofree(scene)
 	await wait_seconds(1.5)
 	
 	# Check results
-	assert_true(FileAccess.file_exists(project_path + "file1.txt"))
-	assert_true(FileAccess.file_exists(project_path + "file1-copy.txt"))
-	assert_true(FileAccess.file_exists(project_path + "EmptyDir/file1-copy.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "file1.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "file1-copy.txt"))
+	assert_true(FileAccess.file_exists(PROJECTS[1] + "EmptyDir/file1-copy.txt"))
 
 	# Cleanup
-	DirAccess.remove_absolute(project_path + "file1-copy.txt")
-	DirAccess.remove_absolute(project_path + "EmptyDir/file1-copy.txt")
+	DirAccess.remove_absolute(PROJECTS[1] + "file1-copy.txt")
+	DirAccess.remove_absolute(PROJECTS[1] + "EmptyDir/file1-copy.txt")
